b1daf33a2ef01e1c8c5214ab5b1c3e93889cbde7
alirezafarin <alirafarinzad@gmail.com>
2023-10-28 00:20:28 +0330

    feat: added form inputs

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -5,22 +5,70 @@ const onSubmit = () => {
 </script>
 
 <template>
-  <div class="form-container">
-    <b-form @submit="onSubmit">
-      <b-form-group
-        id="input-group-1"
-        label="Email address:"
-        label-for="input-1"
-        description="We'll never share your email with anyone else."
-      >
-        <b-form-input id="input-1" type="email" placeholder="Enter email" required></b-form-input>
-      </b-form-group>
-    </b-form>
+  <div class="container">
+    <div class="form-container px-3">
+      <h1 class="title mt-4">Register</h1>
+      <b-form @submit="onSubmit">
+        <b-form-group id="example-input-group-1" label="Name" label-for="example-input-1">
+          <b-form-input
+            id="example-input-1"
+            name="example-input-1"
+            :state="null"
+            aria-describedby="input-1-live-feedback"
+          ></b-form-input>
+
+          <b-form-invalid-feedback id="input-1-live-feedback"
+            >This is a required field and must be at least 3 characters.</b-form-invalid-feedback
+          >
+        </b-form-group>
+        <b-form-group id="example-input-group-1" label="Name" label-for="example-input-1">
+          <b-form-input
+            id="example-input-1"
+            name="example-input-1"
+            :state="null"
+            aria-describedby="input-1-live-feedback"
+          ></b-form-input>
+
+          <b-form-invalid-feedback id="input-1-live-feedback"
+            >This is a required field and must be at least 3 characters.</b-form-invalid-feedback
+          >
+        </b-form-group>
+        <b-form-group id="example-input-group-1" label="Name" label-for="example-input-1">
+          <b-form-input
+            id="example-input-1"
+            name="example-input-1"
+            :state="null"
+            aria-describedby="input-1-live-feedback"
+          ></b-form-input>
+
+          <b-form-invalid-feedback id="input-1-live-feedback"
+            >This is a required field and must be at least 3 characters.</b-form-invalid-feedback
+          >
+        </b-form-group>
+      </b-form>
+    </div>
   </div>
 </template>
 
 <style scoped>
+.container {
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 .form-container {
   background-color: #eceeef;
+  width: 100%;
+  max-width: 450px;
+  border-radius: 4px;
+}
+
+.title {
+  font-family: HelveticaNeue;
+  font-size: 47px;
+  line-height: 1.19;
+  color: #707070;
+  text-align: center;
 }
 </style>
