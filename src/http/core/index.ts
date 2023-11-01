49997de6939bcf27cf6f4b9319add440414f0b59
alirezafarin <alirafarinzad@gmail.com>
2023-11-01 14:10:59 +0330

    config: added axios config

---
+++ src/http/core/index.ts
@@ -0,0 +1,31 @@
+import axios, { AxiosError, type AxiosRequestConfig, type AxiosResponse } from 'axios'
+import type { IResponse } from '../model'
+
+const axiosInstance = axios.create({ timeout: 10000 })
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    // config.headers["token"] = getToken();
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
+
+axiosInstance.interceptors.response.use(
+  function (response: AxiosResponse) {
+    return response
+  },
+  function (error: AxiosError) {
+    // ToastErrorMessage(error?.message ?? dictionary.errors.general)
+    return Promise.reject()
+  }
+)
+
+export const http = {
+  post: <T = any[]>(url: string, data?: any, config?: any) =>
+    axiosInstance.post<IResponse<T>>(url, data, config),
+  get: <T = any[], D = any>(url: string, config?: AxiosRequestConfig<D>) =>
+    axiosInstance.get<IResponse<T>>(url, config)
+}
---
+++ src/http/model/index.ts
@@ -0,0 +1,3 @@
+export interface IResponse<T> {
+  res: T;
+}
