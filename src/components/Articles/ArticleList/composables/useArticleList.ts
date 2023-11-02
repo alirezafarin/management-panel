fec80c90d30814b53e35f7238e1b3a761d74bb57
alirezafarin <alirafarinzad@gmail.com>
2023-11-02 12:13:21 +0330

    refactor: improved service naming

--- src/components/Articles/ArticleList/composables/useArticleList.ts
+++ src/components/Articles/ArticleList/composables/useArticleList.ts
@@ -1,11 +1,11 @@
 import { useQuery } from '@tanstack/vue-query';
 import { type Ref } from 'vue';
-import { articlesService } from '../services';
+import { fetchArticlesService } from '../services';
 
 export function useArticleList(currentPage: Ref<string>) {
   const { data, isFetching } = useQuery({
     queryKey: ['articles', currentPage],
-    queryFn: ({ signal }) => articlesService(currentPage, signal),
+    queryFn: ({ signal }) => fetchArticlesService(currentPage, signal),
     initialData: { articlesCount: 0, articles: [] }
   });
 
--- src/components/Articles/ArticleList/services/fetchArticlesService.ts
+++ src/components/Articles/ArticleList/services/fetchArticlesService.ts
@@ -2,7 +2,7 @@ import { http } from '@/http';
 import type { IArticles } from '.';
 import { type Ref } from 'vue';
 
-export const articlesService = async (page: Ref<string>, signal: AbortSignal) => {
+export const fetchArticlesService = async (page: Ref<string>, signal: AbortSignal) => {
   const { data: response } = await http.get<IArticles>('/articles', {
     params: { page: page.value },
     signal
