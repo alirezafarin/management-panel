f3ef0fa2241e76250773493320020e5ec0659a4c
alirezafarin <alirafarinzad@gmail.com>
2023-11-03 00:54:09 +0330

    feat: added custom-overlay

---
+++ src/components/DesignSystem/components/CustomOverlay.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{ show?: boolean }>();
+</script>
+
+<template>
+  <b-overlay class="custom-overlay" spinner-variant="primary" :show="show" rounded="sm"
+    ><slot></slot
+  ></b-overlay>
+</template>
+
+<style scoped lang="scss">
+.custom-overlay {
+  height: calc(100vh - $navbar-height);
+}
+</style>
--- src/views/EditView.vue
+++ src/views/EditView.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 import CreateEditArticle from '@/components/Articles/CreateEditArticle/CreateEditArticle.vue';
 import { useEditArticle } from '@/components/Articles/CreateEditArticle/composables';
-import LoadingState from '@/components/DesignSystem/components/LoadingState.vue';
+import CustomOverlay from '@/components/DesignSystem/components/CustomOverlay.vue';
 import { en } from '@/dictionary/en';
 
 const { isFetching } = useEditArticle();
 </script>
 
 <template>
-  <LoadingState v-if="isFetching" />
-  <CreateEditArticle v-else :title="en.articles.editArticle" />
+  <CustomOverlay :show="isFetching">
+    <CreateEditArticle :title="en.articles.editArticle" />
+  </CustomOverlay>
 </template>
