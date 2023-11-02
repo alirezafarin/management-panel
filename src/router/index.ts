65440860c8833dd2c5a6db6fcd76f7cb2fe9b898
alirezafarin <alirafarinzad@gmail.com>
2023-11-03 01:38:11 +0330

    refactor: convert all view imports to dynamic imports for lazy loading

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,5 @@
-import LoginView from '@/views/LoginView.vue';
-import RegisterView from '@/views/RegisterView.vue';
-import NavigationView from '@/views/NavigationView.vue';
-import CreateView from '@/views/CreateView.vue';
-import ArticlesView from '@/views/ArticlesView.vue';
-import { createRouter, createWebHistory } from 'vue-router';
 import { isAuthenticated } from '@/helper';
-import EditView from '@/views/EditView.vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,49 +7,49 @@ const router = createRouter({
     {
       path: '/register',
       name: 'register',
-      component: RegisterView,
-      meta: { authPath: true }
+      component: import('@/views/RegisterView.vue'),
+      meta: { authPath: true },
     },
     {
       path: '/login',
       name: 'login',
-      component: LoginView,
-      meta: { authPath: true }
+      component: import('@/views/LoginView.vue'),
+      meta: { authPath: true },
     },
     {
       path: '/articles/create',
       name: 'createArticle',
       components: {
-        navigation: NavigationView,
-        main: CreateView
+        navigation: import('@/views/NavigationView.vue'),
+        main: import('@/views/CreateView.vue'),
       },
-      meta: { requiresAuth: true }
+      meta: { requiresAuth: true },
     },
     {
       path: '/articles/edit/:slug',
       name: 'editArticle',
       components: {
-        navigation: NavigationView,
-        main: EditView
+        navigation: import('@/views/NavigationView.vue'),
+        main: import('@/views/EditView.vue'),
       },
-      meta: { requiresAuth: true }
+      meta: { requiresAuth: true },
     },
     {
       path: '/articles/:page?',
       name: 'home',
       components: {
-        navigation: NavigationView,
-        main: ArticlesView
+        navigation: import('@/views/NavigationView.vue'),
+        main: import('@/views/ArticlesView.vue'),
       },
-      meta: { requiresAuth: true }
+      meta: { requiresAuth: true },
     },
     {
-      path: '/',
-      redirect: (to) => {
+      path: '/:notFound',
+      redirect: () => {
         return { name: 'home' };
-      }
-    }
-  ]
+      },
+    },
+  ],
 });
 
 router.beforeEach((to, from) => {
@@ -63,12 +57,12 @@ router.beforeEach((to, from) => {
   if (to.meta.requiresAuth && !isAuth) {
     return {
       name: 'login',
-      query: { redirect: to.fullPath }
+      query: { redirect: to.fullPath },
     };
   }
   if (to.meta.authPath && isAuth) {
     return {
-      path: from.fullPath
+      path: from.fullPath,
     };
   }
 });
